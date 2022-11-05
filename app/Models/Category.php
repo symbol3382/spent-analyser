a5116c0ee4b45756ad256d692e67b4e4793c83e3
Gourav <gourav.verma@kct-technologies.com>
2022-11-05 16:54:54 +0530

    Added Laravel Auto Doc Generator

--- app/Models/Category.php
+++ app/Models/Category.php
@@ -2,9 +2,31 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Builder;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Support\Carbon;
 
+/**
+ * App\Models\Category
+ *
+ * @property int $id
+ * @property string $category_name
+ * @property int|null $parent_category_id
+ * @property int $created_by
+ * @property Carbon|null $created_at
+ * @property Carbon|null $updated_at
+ * @method static Builder|Category newModelQuery()
+ * @method static Builder|Category newQuery()
+ * @method static Builder|Category query()
+ * @method static Builder|Category whereCategoryName($value)
+ * @method static Builder|Category whereCreatedAt($value)
+ * @method static Builder|Category whereCreatedBy($value)
+ * @method static Builder|Category whereId($value)
+ * @method static Builder|Category whereParentCategoryId($value)
+ * @method static Builder|Category whereUpdatedAt($value)
+ * @mixin \Eloquent
+ */
 class Category extends Model {
     use HasFactory;
 
--- app/Models/Transaction.php
+++ app/Models/Transaction.php
@@ -2,10 +2,39 @@
 
 namespace App\Models;
 
+use Eloquent;
+use Illuminate\Database\Eloquent\Builder;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Support\Carbon;
 
+/**
+ * App\Models\Transaction
+ *
+ * @property int $id
+ * @property int $amount
+ * @property int $user_id
+ * @property string $transaction_type
+ * @property Carbon $transaction_time
+ * @property int $category_id
+ * @property Carbon|null $created_at
+ * @property Carbon|null $updated_at
+ * @property-read Category $category
+ * @property-read User $user
+ * @method static Builder|Transaction newModelQuery()
+ * @method static Builder|Transaction newQuery()
+ * @method static Builder|Transaction query()
+ * @method static Builder|Transaction whereAmount($value)
+ * @method static Builder|Transaction whereCategoryId($value)
+ * @method static Builder|Transaction whereCreatedAt($value)
+ * @method static Builder|Transaction whereId($value)
+ * @method static Builder|Transaction whereTransactionTime($value)
+ * @method static Builder|Transaction whereTransactionType($value)
+ * @method static Builder|Transaction whereUpdatedAt($value)
+ * @method static Builder|Transaction whereUserId($value)
+ * @mixin Eloquent
+ */
 class Transaction extends Model {
     use HasFactory;
 
